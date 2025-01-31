62424f280a0785a38d55f266869e9606b3c620c7
Saheed Shittu <saheedsh2@gmail.com>
2025-01-31 16:11:51 +0100

    Added twitter, facebook and Instagram to the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2025 Mosh Headphones All Rights Reserve</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+      </p>
+    </div>
   )
 }
 
